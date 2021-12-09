60371e006f6c0e9b90a5e68d004ef2f28cc10d5c
Aleksey Shelementev <rushelex@gmail.com>
2021-12-09 20:56:36 +0500

    fix: adds config for production
    Signed-off-by: Aleksey Shelementev <rushelex@gmail.com>

---
+++ config/env/production/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: 'http://api.chaoscontrol.xyz',
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '845bffa72d434e7d0ad67ba7073b1624'),
+    },
+  },
+});
